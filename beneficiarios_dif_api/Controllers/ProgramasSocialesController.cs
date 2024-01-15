1775b39525750225a57ea15b9281aa1df00716ff
Gabriel <g27r02@gmail.com>
2024-01-15 12:26:47 -0600

    ✨ PROGRAMAS SOCIALES

--- beneficiarios_dif_api/Controllers/ProgramasSocialesController.cs
+++ beneficiarios_dif_api/Controllers/ProgramasSocialesController.cs
@@ -38,5 +38,93 @@ namespace beneficiarios_dif_api.Controllers
             return Ok(mapper.Map<List<ProgramaSocialDTO>>(ProgramasSociales));
         }
 
+        [HttpPost("crear")]
+        public async Task<ActionResult> Post(ProgramaSocialDTO dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var existePrograma = await context.ProgramasSociales.AnyAsync(n => n.Nombre == dto.Nombre);
+
+            if (existePrograma)
+            {
+                return Conflict();
+            }
+
+            var programa = mapper.Map<ProgramaSocial>(dto);
+
+            context.Add(programa);
+
+            try
+            {
+                await context.SaveChangesAsync();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500);
+            }
+        }
+
+        [HttpDelete("eliminar/{id:int}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var programa = await context.ProgramasSociales.FindAsync(id);
+
+            if (programa == null)
+            {
+                return NotFound();
+            }
+
+            context.ProgramasSociales.Remove(programa);
+            await context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPut("actualizar/{id:int}")]
+        public async Task<ActionResult> Put(int id, [FromBody] ProgramaSocialDTO dto)
+        {
+            if (id != dto.Id)
+            {
+                return BadRequest("El ID de la ruta y el ID del objeto no coinciden");
+            }
+
+            var programa = await context.ProgramasSociales.FindAsync(id);
+
+            if (programa == null)
+            {
+                return NotFound();
+            }
+
+            mapper.Map(dto, programa);
+
+            context.Update(programa);
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProgramaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        private bool ProgramaExists(int id)
+        {
+            return context.ProgramasSociales.Any(e => e.Id == id);
+        }
     }
 }
