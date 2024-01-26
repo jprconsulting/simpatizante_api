58b2f2783ced749560aafe74f5175e946a66606f
Cristian Carreto <carreto154@gmail.com>
2024-01-26 13:07:27 -0600

    🐛 FIX LOGIN

--- beneficiarios_dif_api/Controllers/AuthController.cs
+++ beneficiarios_dif_api/Controllers/AuthController.cs
@@ -1,38 +1,45 @@
-﻿//using beneficiarios_dif_api.DTOs;
-//using beneficiarios_dif_api.Services;
-//using Microsoft.AspNetCore.Http;
-//using Microsoft.AspNetCore.Mvc;
+﻿using beneficiarios_dif_api.DTOs;
+using beneficiarios_dif_api.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
 
-//namespace beneficiarios_dif_api.Controllers
-//{
-//    [Route("api/auth")]
-//    [ApiController]
-//    public class AuthController : ControllerBase
-//    {
-//        private readonly IAuthorizationService authService;
+namespace conoceles_api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthorizationService authService;
+        private readonly ILogger<AuthController> logger;
 
-//        public AuthController(IAuthorizationService authService)
-//        {
-//            this.authService = authService;
-//        }
+        public AuthController(IAuthorizationService authService, ILogger<AuthController> logger)
+        {
+            this.authService = authService;
+            this.logger = logger;
+        }
 
-//        [HttpPost("login")]
-//        public async Task<IActionResult> Login(AppUserDTO dto)
-//        {
-//            try
-//            {
-//                var result = await authService.ValidateUser(dto);
-//                if (result == null)
-//                    return Unauthorized();
+        [HttpPost("login")]
+        public async Task<IActionResult> Login(AppUserDTO dto)
+        {
+            try
+            {
+                var result = await authService.ValidateUser(dto);
+                if (result == null)
+                    return Unauthorized();
 
-//                return Ok(result);
-//            }
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                // Loguea la excepción para obtener más detalles
+                logger.LogError(ex, "Error durante la autenticación del usuario.");
 
-//            catch (Exception ex)
-//            {
-//                return StatusCode(500);
-//            }
-//        }
-
-//    }
-//}
+                // Devuelve una respuesta con detalles del error
+                return StatusCode(500, new { ErrorMessage = "Ocurrió un error durante la autenticación del usuario.", Exception = ex.Message });
+            }
+        }
+    }
+}
