9b77e1d961c4a282531bf92e3ac9dd309a74531f
Gabriel <g27r02@gmail.com>
2024-03-06 12:53:14 -0600

    🐛 FIX BUG DISTRIBUCION

--- simpatizantes_api/DTOs/ComunidadDTO.cs
+++ simpatizantes_api/DTOs/ComunidadDTO.cs
@@ -4,7 +4,7 @@ namespace simpatizantes_api.DTOs
 {
     public class ComunidadDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Nombre { get; set; }
         public MunicipioDTO Municipio { get; set; }
 
