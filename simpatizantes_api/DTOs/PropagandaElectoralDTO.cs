1842cf0f59f4967d6cb24ee7d5fdbca130e05ddd
Gabriel <g27r02@gmail.com>
2024-03-08 14:59:25 -0600

    🐛 FIX ID PROPAGANDA

--- simpatizantes_api/DTOs/PropagandaElectoralDTO.cs
+++ simpatizantes_api/DTOs/PropagandaElectoralDTO.cs
@@ -2,7 +2,7 @@
 {
     public class PropagandaElectoralDTO
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public string Folio { get; set; }
         public string Comentarios { get; set; }
         public decimal Latitud { get; set; }
